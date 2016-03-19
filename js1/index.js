443342f93a1f47a254856ebfc31c6863a37a9e0a
renxiaoxin <1134280154Qqq.com>
2016-03-19 20:09:47 +0800

    增加pc端的项目

---
+++ js1/index.js
@@ -0,0 +1,349 @@
+//数据绑定
+function bindData() {
+    var leftTop = document.getElementById("left_top");
+    var str = "";
+    for (var i = 0; i < dataAry.length; i++) {
+        var cur = dataAry[i];
+        str += "<div class='menu_box'>";
+        str += "<div class='menu_box_title'>";
+        str += "<h2>" + cur['title'] + "<span></span></h2>";
+        for (var j = 0; j < cur['items'].length; j++) {
+            var curA = cur['items'][j];
+            str += "<a href='javascript:;'>" + curA + "</a>";
+        }
+        str += "</div>";
+        str += "<div class='menu_box_list'>";
+        for(var m=0;m<cur['pushes'].length;m++){
+            var curM=cur['pushes'][m];
+            str += "<dl>";
+            str += "<dt>";
+            str += "<a href='javascript:;'>" + curM['pushTitle'] + "</a>";
+            str += "</dt>";
+            str += "<dd>";
+            for (var n = 0; n < curM['pushItems'].length; n++) {
+                var curP=curM['pushItems'][n];
+                str += "<a href='javascript:;'>" +  curP+ "</a>";
+            }
+            str += "</dd>";
+            str += "</dl>";
+        }
+
+        str += "</div>";
+        str += "</div>";
+    }
+    leftTop.innerHTML = str;
+}
+//左侧导航
+(function () {
+    bindData();
+    var leftTop = document.getElementById("left_top");
+    var menuBox = utils.getElementsByClass("menu_box", leftTop);
+    var menuBoxTitle = utils.getElementsByClass("menu_box_title", leftTop);
+    var menuBoxList = utils.getElementsByClass("menu_box_list", leftTop);
+    //console.log(menuBox,menuBoxList);
+    for (var i = 0; i < menuBoxTitle.length; i++) {
+        ~function (i) {
+            var curBox=menuBoxTitle[i];
+            curBox.onmouseover = function () {
+                menuBoxList[i].style.display = "block";
+                menuBoxList[i].style.top = -i*5+"px";
+                menuBoxTitle[i].style.background = "#fff";
+                menuBoxTitle[i].style.border = "2px solid #c9cbce";
+                menuBoxTitle[i].style.borderRight = "2px solid transparent";
+                //menuBoxList[i].style.zIndex=9;
+                menuBoxTitle[i].style.zIndex=103;
+            };
+            curBox.onmouseout = function () {
+                menuBoxList[i].style.display = "none";
+                menuBoxTitle[i].style.background = "#fafafa";
+                menuBoxTitle[i].style.border = "2px solid #fafafa";
+                menuBoxTitle[i].style.zIndex=101;
+            };
+        }(i);
+    }
+})();
+//轮播图
+(function () {
+    //->数据源
+    //->定义几个初始的变量
+    var autoTimer = null, step = 0, count = 3;
+
+    var topBannerL = document.getElementById("top_banner_l"), imgList1 = topBannerL.getElementsByTagName("img");
+    var bannerControl = document.getElementById("banner_control"), oLis = bannerControl.getElementsByTagName("li");
+    var imgList2 = bannerControl.getElementsByTagName("img");
+    var oEm = document.getElementById("em1");
+    //->数据绑定
+    //->图片延迟加载
+    window.setTimeout(lazyImg, 500);
+    function lazyImg() {
+        for (var i = 0; i < imgList1.length; i++) {
+            ~function (i) {
+                var curImg = imgList1[i];
+                var oImg = new Image;
+                oImg.src = curImg.getAttribute("trueImg");
+                oImg.onload = function () {
+                    curImg.src = this.src;
+                    curImg.style.display = "block";
+                    animate(curImg, {opacity: 1}, 500);
+                }
+            }(i);
+        }
+    }
+
+    //->实现图片控制样式的选中
+    function selectTip() {
+        var tempStep = step;
+        tempStep >= oLis.length ? tempStep = 0 : null;
+        for (var i = 0; i < oLis.length; i++) {
+            oEm.style.top = 55 * (tempStep) + "px";
+            imgList2[i].className = i === tempStep ? "bg" : null;
+
+        }
+    }
+
+    //->实现右侧小图片的划过控制切换轮播图
+    tipMove();
+    function tipMove() {
+        for (var i = 0; i < oLis.length; i++) {
+            var curTip = oLis[i];
+            curTip.index = i;
+            curTip.onmouseover = function () {
+                window.clearInterval(autoTimer);
+                step = this.index;
+                animate(topBannerL, {top: -step * 160}, 300);
+                selectTip();
+                autoTimer = window.setInterval(autoMove, 2000);
+            }
+        }
+    }
+
+    //->实现自动轮播
+    function autoMove() {
+        step++;
+        if (step > count) {
+            step = 1;
+            topBannerL.style.top = 0;
+        }
+        animate(topBannerL, {top: -step * 160}, 300);
+        selectTip();
+    }
+
+    autoTimer = window.setInterval(autoMove, 2000);
+})();
+//鼠标滑过
+(function () {
+    var allMove = document.getElementById("all_move"), oLis = allMove.getElementsByTagName("li"), imgList = allMove.getElementsByTagName("img");
+    var oDivs = allMove.getElementsByTagName("div");
+
+    for (var i = 0; i < oLis.length; i++) {
+        var cur = oLis[i];
+        ~function (i) {
+            cur.onmouseenter = function (e) {
+                e = e || window.event;
+                //console.log(this);
+                e.target = e.target || e.srcElement;
+                var left = utils.offset(imgList[i]).left;
+                var top = utils.offset(imgList[i]).top;
+                var liLeft = imgList[i].offsetWidth;
+                var liTop = imgList[i].offsetHeight;
+                if (e.pageX < liLeft || e.pageY < liTop || e.pageX > (liLeft + imgList[i].offsetWidth) || e.pageY > (liTop + oLis[i].offsetHeight)) {
+                    e.target.style.display = "block";
+                    e.target.style.left = 0;
+                    e.target.style.top = 0;
+                }
+
+            };
+            cur.onmousemove = function (e) {
+                e = e || window.event;
+                if (oDivs[i]) {
+                    oDivs[i].style.left = 0;
+                    oDivs[i].style.top = 0;
+                }
+            };
+            cur.onmouseleave = function (e) {
+                e = e || window.event;
+                oDivs[i].style.display = "block";
+
+                var left = utils.offset(oDivs[i]).left;
+                var top = utils.offset(oDivs[i]).top;
+                var liLeft = utils.offset(imgList[i]).left;
+                var liTop = utils.offset(imgList[i]).top;
+
+                if (e.pageX < liLeft && (e.pageY - top) > 0 && (e.pageY - top) < 112) {
+                    oDivs[i].style.left = "-100%";
+                    oDivs[i].style.top = 0;
+
+                } else if (e.pageY < liTop && e.pageX - liLeft > 0 && e.pageX - liLeft < 112) {
+                    oDivs[i].style.left = 0;
+                    oDivs[i].style.top = "-100%";
+
+                } else if (e.pageX > (liLeft + imgList[i].offsetWidth) && e.pageY - top > 0 && e.pageY - top < 112) {
+                    oDivs[i].style.left = "100%";
+                    oDivs[i].style.top = 0;
+
+                } else
+                //if (e.pageY > (liTop + oLis[i].offsetHeight) && e.pageX - left > 0 && e.pageX - left < 112)
+                {
+                    oDivs[i].style.left = 0;
+                    oDivs[i].style.top = "100%";
+
+                }
+            }
+        }(i);
+    }
+})();
+//回到顶部
+(function () {
+    var scrollObj = new scrollModel({
+        selector: "backtop",
+        duration: 500
+    });
+    scrollObj.init();
+})();
+//职位选项卡
+(function(){
+    var jobTab=document.getElementById("job_tab");
+    jobTab.onclick = function (e) {
+        //->e:事件对象
+        e = e || window.event;
+        var tar = e.target || e.srcElement;
+        //->如果tar是我们点击的这个页卡的li
+        if (tar.tagName="li") {
+            changeTab.call(tar);
+        }
+    };
+})();
+//选项卡方法
+function changeTab() {
+    utils.addClass(this, "job_tab_select");
+    var siblingLi = utils.siblings(this);
+    for (var i = 0; i < siblingLi.length; i++) {
+        utils.removeClass(siblingLi[i], "job_tab_select");
+    }
+
+    var divList = utils.nextAll(this.parentNode), index = utils.getIndex(this);
+    for (i = 0; i < 2; i++) {
+        var curDiv = divList[i];
+        i === index ? utils.addClass(curDiv, "tab_select") : utils.removeClass(curDiv, "tab_select");
+    }
+}
+(function(){
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*     var left = utils.offset(imgList[i]).left;
+ var top = utils.offset(imgList[i]).top;
+ var liLeft = imgList[i].offsetWidth;
+ var liTop = imgList[i].offsetHeight;
+
+ this["mouseX"] = e.pageX-left;
+ this["mouseY"] = e.pageY-top;
+
+ if (this.mouseX<=this.mouseY&&this.mouseX+this.mouseY<liTop) {
+ utils.setCss(e.target, "left", "-100%");
+ utils.setCss(e.target, "top", 0);
+ animate(e.target,{left:0,top:0});
+ }
+ if (this.mouseX<=this.mouseY&&this.mouseX+this.mouseY>liTop) {
+ utils.setCss(e.target, "left", 0);
+ utils. setCss(e.target, "top", "100%");
+ animate(e.target,{left:0,top:0});
+ }
+ if (this.mouseX>=this.mouseY&&this.mouseX+this.mouseY<liTop) {
+ utils.setCss(e.target, "left", 0);
+ utils.setCss(e.target, "top", "-100%");
+ animate(e.target,{left:0,top:0});
+ }
+ if (this.mouseX>=this.mouseY&&this.mouseX+this.mouseY>liTop) {
+ utils.setCss(e.target, "left", "100%");
+ utils. setCss(e.target, "top", 0);
+ animate(e.target,{left:0,top:0});
+ }
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js1/json1.js
@@ -0,0 +1,170 @@
+var dataAry = [{
+    "title": "技术",
+    "items": ["Java", "PHP", "C", "C++", "Android", "iOS", "测试", "前端开发", "技术经理", "技术总监", "架构师", "CTO"],
+    "pushes": [{
+        "pushTitle": "后端开发",
+        "pushItems": ["Java", "Python", "PHP", ".NET", "C#", "C++", "C", "VB", "Delphi", "Perl", "Ruby", "Hadoop", "Node.js", "数据挖掘", "自然语言处理", "搜索算法", "精准推荐", "全栈工程师", "Go", "ASP", "Shell", "后端开发其它"]
+    }, {"pushTitle": "移动开发",
+        "pushItems": ["HTML5", "Android", "iOS", "WP", "移动开发其它"]}, {
+        "pushTitle": "前端开发",
+        "pushItems": ["web前端", "Flash", "html5", "JavaScript", "U3D", "COCOS2D-X", "前端开发其它"]
+    }, {
+        "pushTitle": "测试",
+        "pushItems": ["测试工程师", "自动化测试", "功能测试", "性能测试", "测试开发", "游戏测试", "白盒测试","灰盒测试","黑盒测试","手机测试","硬件测试","测试经理","测试其它"]
+    }, {
+        "pushTitle": "运维",
+        "pushItems": ["运维工程师", "运维开发工程师", "系统工程师", "IT支持", "IDC", "CDN", "F5","系统管理员","病毒分析","WEB安全","网络安全","系统安全","运维经理","运维其它"]
+    },{
+        "pushTitle": "DBA",
+        "pushItems": ["MySQL", "SQLServer", "Oracle", "DB2", "MongoDB", "ETL", "Hive","数据仓库","DBA其它"]
+    },{
+        "pushTitle": "高端职位",
+        "pushItems": ["技术经理", "技术总监", "架构师", "CTO", "运维总监", "技术合伙人", "项目总监","测试总监","安全专家","高端技术职位其它"]
+    },{
+        "pushTitle": "项目管理",
+        "pushItems": ["项目经理", "项目助理"]
+    },{
+        "pushTitle": "硬件开发",
+        "pushItems": ["硬件", "嵌入式", "自动化", "单片机", "电路设计", "驱动开发", "系统集成","FPGA开发","DSP开发","ARM开发","PCB工艺","模具设计","热传导","材料工程师","精益工程师","射频工程师","硬件开发其它"]
+    },{
+        "pushTitle": "企业软件",
+        "pushItems": ["实施工程师", "售前工程师", "售后工程师", "BI工程师", "企业软件其它"]
+    }]
+}, {
+    "title": "产品",
+    "items": ["产品总监", "产品经理", "移动产品经理", "游戏策划"],
+    "pushes": [{
+        "pushTitle": "后端开发",
+        "pushItems": ["Java", "Python", "PHP", ".NET", "C#", "C++", "C", "VB", "Delphi", "Perl", "Ruby", "Hadoop", "Node.js", "数据挖掘", "自然语言处理", "搜索算法", "精准推荐", "全栈工程师", "Go", "ASP", "Shell", "后端开发其它"]
+    }, {"pushTitle": "移动开发", "pushItems": ["HTML5", "Android", "iOS", "WP", "移动开发其它"]}, {
+        "pushTitle": "前端开发",
+        "pushItems": ["web前端", "Flash", "html5", "JavaScript", "U3D", "COCOS2D-X", "前端开发其它"]
+    }]
+}, {
+    "title": "设计",
+    "items": ["设计总监", "UI", "UE", "交互设计", "数据分析"],
+    "pushes": [{
+        "pushTitle": "后端开发",
+        "pushItems": ["Java", "Python", "PHP", ".NET", "C#", "C++", "C", "VB", "Delphi", "Perl", "Ruby", "Hadoop", "Node.js", "数据挖掘", "自然语言处理", "搜索算法", "精准推荐", "全栈工程师", "Go", "ASP", "Shell", "后端开发其它"]
+    }, {"pushTitle": "移动开发", "pushItems": ["HTML5", "Android", "iOS", "WP", "移动开发其它"]}, {
+        "pushTitle": "前端开发",
+        "pushItems": ["web前端", "Flash", "html5", "JavaScript", "U3D", "COCOS2D-X", "前端开发其它"]
+    }]
+}, {
+    "title": "运营",
+    "items": ["新媒体运营", "数据运营", "运营总监", "COO", "编辑"],
+    "pushes": [{
+        "pushTitle": "后端开发",
+        "pushItems": ["Java", "Python", "PHP", ".NET", "C#", "C++", "C", "VB", "Delphi", "Perl", "Ruby", "Hadoop", "Node.js", "数据挖掘", "自然语言处理", "搜索算法", "精准推荐", "全栈工程师", "Go", "ASP", "Shell", "后端开发其它"]
+    }, {"pushTitle": "移动开发", "pushItems": ["HTML5", "Android", "iOS", "WP", "移动开发其它"]}, {
+        "pushTitle": "前端开发",
+        "pushItems": ["web前端", "Flash", "html5", "JavaScript", "U3D", "COCOS2D-X", "前端开发其它"]
+    }]
+}, {
+    "title": "市场与销售",
+    "items": ["市场推广", "市场总监", "市场策划", "BD", "销售总监"],
+    "pushes": [{
+        "pushTitle": "后端开发",
+        "pushItems": ["Java", "Python", "PHP", ".NET", "C#", "C++", "C", "VB", "Delphi", "Perl", "Ruby", "Hadoop", "Node.js", "数据挖掘", "自然语言处理", "搜索算法", "精准推荐", "全栈工程师", "Go", "ASP", "Shell", "后端开发其它"]
+    }, {"pushTitle": "移动开发", "pushItems": ["HTML5", "Android", "iOS", "WP", "移动开发其它"]}, {
+        "pushTitle": "前端开发",
+        "pushItems": ["web前端", "Flash", "html5", "JavaScript", "U3D", "COCOS2D-X", "前端开发其它"]
+    }]
+}, {
+    "title": "职能",
+    "items": ["HR", "行政", "会计", "出纳"],
+    "pushes": [{
+        "pushTitle": "后端开发",
+        "pushItems": ["Java", "Python", "PHP", ".NET", "C#", "C++", "C", "VB", "Delphi", "Perl", "Ruby", "Hadoop", "Node.js", "数据挖掘", "自然语言处理", "搜索算法", "精准推荐", "全栈工程师", "Go", "ASP", "Shell", "后端开发其它"]
+    }, {"pushTitle": "移动开发", "pushItems": ["HTML5", "Android", "iOS", "WP", "移动开发其它"]}, {
+        "pushTitle": "前端开发",
+        "pushItems": ["web前端", "Flash", "html5", "JavaScript", "U3D", "COCOS2D-X", "前端开发其它"]
+    }]
+}, {
+    "title": "金融",
+    "items": ["投资", "融资", "并购", "风控"],
+    "pushes": [{
+        "pushTitle": "后端开发",
+        "pushItems": ["Java", "Python", "PHP", ".NET", "C#", "C++", "C", "VB", "Delphi", "Perl", "Ruby", "Hadoop", "Node.js", "数据挖掘", "自然语言处理", "搜索算法", "精准推荐", "全栈工程师", "Go", "ASP", "Shell", "后端开发其它"]
+    }, {"pushTitle": "移动开发", "pushItems": ["HTML5", "Android", "iOS", "WP", "移动开发其它"]}, {
+        "pushTitle": "前端开发",
+        "pushItems": ["web前端", "Flash", "html5", "JavaScript", "U3D", "COCOS2D-X", "前端开发其它"]
+    }]
+}];
+
+var indexCareers = [{
+    "title": "Node.js",
+    "add": "北京",
+    "time": "14:54",
+    "company": "洋葱教学",
+    "salary": "20k-30k",
+    "expre": "3-5年",
+    "bachlor": "学历不限",
+    "area": "移动互联网",
+    "dis": "教育",
+    "grow": "成长型(B轮)",
+    "info": "丰厚期权、超长年假、年度体检、免费早午餐",
+    "tags": ["产品领先行业", "关注学习资源"]
+}, {
+    "title": "数据挖掘",
+    "add": "北京",
+    "time": "17:47",
+    "company": "百分点",
+    "salary": "15k-30k",
+    "expre": "3-5年",
+    "bachlor": "本科",
+    "dis": "数据服务",
+    "grow": "成熟型(D轮及以上)",
+    "info": "五险一金 全员持股 没有天花板",
+    "tags": ["节日礼物", "年底双薪", "定期体检"]
+}, {
+    "title": "Node.js",
+    "add": "北京",
+    "time": "14:54",
+    "company": "洋葱教学",
+    "salary": "20k-30k",
+    "expre": "3-5年",
+    "bachlor": "学历不限",
+    "area": "移动互联网",
+    "dis": "教育",
+    "grow": "成长型(B轮)",
+    "info": "丰厚期权、超长年假、年度体检、免费早午餐",
+    "tags": ["产品领先行业", "关注学习资源"]
+}, {
+    "title": "数据挖掘",
+    "add": "北京",
+    "time": "17:47",
+    "company": "百分点",
+    "salary": "15k-30k",
+    "expre": "3-5年",
+    "bachlor": "本科",
+    "dis": "数据服务",
+    "grow": "成熟型(D轮及以上)",
+    "info": "五险一金 全员持股 没有天花板",
+    "tags": ["节日礼物", "年底双薪", "定期体检"]
+}, {
+    "title": "Node.js",
+    "add": "北京",
+    "time": "14:54",
+    "company": "洋葱教学",
+    "salary": "20k-30k",
+    "expre": "3-5年",
+    "bachlor": "学历不限",
+    "area": "移动互联网",
+    "dis": "教育",
+    "grow": "成长型(B轮)",
+    "info": "丰厚期权、超长年假、年度体检、免费早午餐",
+    "tags": ["产品领先行业", "关注学习资源"]
+}, {
+    "title": "数据挖掘",
+    "add": "北京",
+    "time": "17:47",
+    "company": "百分点",
+    "salary": "15k-30k",
+    "expre": "3-5年",
+    "bachlor": "本科",
+    "dis": "数据服务",
+    "grow": "成熟型(D轮及以上)",
+    "info": "五险一金 全员持股 没有天花板",
+    "tags": ["节日礼物", "年底双薪", "定期体检"]
+}];
